4d815de561e36b2910cff9bef7ffea89ea774f61
Kailane Pereira <61596675+Kailane-ps@users.noreply.github.com>
2021-01-29 02:52:01 -0300

    CodeTur-Teste

---
+++ CodeTur-Api/CodeTur/CodeTur.Testes/Entidades/UsuarioTestes.cs
@@ -0,0 +1,14 @@
+﻿using CodeTur.Comum.Enum;
+using CodeTur.Dominio.Entidades;
+
+namespace CodeTur.Testes.Entidades
+{
+    public class UsuarioTestes
+    {
+        public void Usuario()
+        {
+            var usuario = new Usuario("Kailane Pereira", "kailaneps@hotmail.com", "123456", EnTipoUsuario.Admin );
+            
+        }
+    }
+}
